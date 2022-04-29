ab81c2c42379c45c9d75d22a89a1c837421913d8
joshgreiff <josh@greiff.org>
2022-04-29 14:23:58 -0400

    updated

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,15 +12,16 @@ router.get('/notes', (req, res) => {
           console.error(err);
           return;
         }
-        res.json(data);
+        res.json(JSON.parse(data));
         
       });
 })
 
 router.post('/notes', ({ body }, res) => {
     const note = body
-    noteArray.push(note)
     
+    noteArray.push(note)
+    console.log(noteArray)
     fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(noteArray, null, 2))
 })
 
